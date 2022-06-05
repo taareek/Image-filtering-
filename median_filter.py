daf1931cf578007fba90e7d1fbd9d8619588bc2d
Tarek Aziz <tarekiub17@gmail.com>
2022-06-06 02:45:02 +0600

    median added

---
+++ median_filter.py
@@ -0,0 +1,33 @@
+from re import A
+import matplotlib.pyplot as plt
+import cv2
+from skimage.filters import median
+from skimage.morphology import disk
+
+img_path = "Images/img_5.png"
+img = cv2.imread(img_path, 0)
+
+# plt.imshow(img, cmap= 'gray')
+# plt.show()
+
+# median using cv2
+c_img = cv2.medianBlur(img, 5)  # 3 is the kernel size
+
+# median using skimage
+s_img = median(img, disk(3), mode= 'constant', cval= 0.0)
+
+fig = plt.figure(figsize=(12,12))
+
+ax1= fig.add_subplot(1, 3, 1)
+ax1.imshow(img, cmap= 'gray')
+ax1.title.set_text("Original image")
+
+ax2= fig.add_subplot(1,3,2)
+ax2.imshow(c_img,cmap= 'gray')
+ax2.title.set_text("Using opencv")
+
+ax3 = fig.add_subplot(1,3,3)
+ax3.imshow(s_img, cmap= 'gray')
+ax3.title.set_text("Using skimage")
+
+plt.show()
